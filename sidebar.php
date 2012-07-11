d5aab6ac33b6a2dc3b255cb575ec38522b54e584
Markus <markus@supermumin.net>
2012-07-11 13:37:41 +0200

    extra sidebar-wrapper
    enklare att cleara floats mha en wrapper

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,8 @@
-
+<div id="sidebars" class="clear">
 	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar") ) : ?>
 	<? else: ?>
 	<?php endif; ?>
 
 	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Second Sidebar") ) : ?>
 	<?php endif; ?>
+</div>

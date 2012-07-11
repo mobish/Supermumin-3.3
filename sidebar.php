0fc938fdd135f40dcae7ccf0bfb1df2068ce950d
Markus <markus@supermumin.net>
2012-07-11 13:38:57 +0200

    tar bort wrappern
    det var visst inte så man skulle göra för att cleara

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,7 @@
-<div id="sidebars" class="clear">
 	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidebar") ) : ?>
 	<? else: ?>
 	<?php endif; ?>
 
 	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Second Sidebar") ) : ?>
 	<?php endif; ?>
-</div>
+<span class="clear"></div>
